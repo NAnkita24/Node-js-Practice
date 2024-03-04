3d70661bbdd5470b6353823be8ef3d6b7f9d6813
NAnkita24 <apjagtap24@gmail.com>
2024-03-04 23:39:52 +0530

    4-March

--- one.js
+++ one.js
@@ -1,4 +1,11 @@
 var a=10;
 var b=20;
 var c=30;
-console.warn(a+b+c);
+//console.warn(a+b+c);
+
+const arr=[1,4,5,8,3,2,9,3,6,5,3];
+
+let result=arr.filter((items)=>{
+return items >4;
+})
+console.warn(result)
