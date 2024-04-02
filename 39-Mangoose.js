0a918e4b9998e8dac585dadaae2e3d8b4aaa14c4
Ankita Jagtap <125237115+NAnkita24@users.noreply.github.com>
2024-04-02 22:58:10 +0530

    Update 39-Mangoose.js

--- 39-Mangoose.js
+++ 39-Mangoose.js
@@ -1,14 +1,17 @@
 const mongoose=require('mongoose')
 
 const main=async()=>{
+    
     await mongoose.connect("mongodb://127.0.0.1:27017/e-com")
    const ecomSchema= new mongoose.Schema({
         name:String
+       
     })
 
     const ecomModel=mongoose.model('videos',ecomSchema)
     let data=new ecomModel({name:"m8"})
     let result= await data.save()
     console.log(result);
+    
 }
 main()
