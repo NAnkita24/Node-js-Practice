99067c12a61c8e0f7de59aa8f720798fc927ed43
Ankita Jagtap <125237115+NAnkita24@users.noreply.github.com>
2024-04-07 19:37:24 +0530

    Update Product.js

--- Product.js
+++ Product.js
@@ -1,9 +1,11 @@
 const mongoose=require('mongoose')
 
 const ecomSchema= new mongoose.Schema({
+    
     name:String,
     brand:String,
     prize:Number
+    
 })
 
 module.exports=mongoose.model('videos',ecomSchema)
